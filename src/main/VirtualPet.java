4b65937dc30e60cd1102525ddc7f794c0589f466
Rbtcoy <rbtcy@outlook.com>
2023-05-31 09:15:40 -0400

    started in on the status method for virtual pet will prob also work on child classes

--- src/main/VirtualPet.java
+++ src/main/VirtualPet.java
@@ -2,11 +2,13 @@ public class VirtualPet {
     private int food = 60;
     private int water = 60;
     private int play = 60;
+    private String name = "";
 
-    public VirtualPet(int food, int water, int play) {
+    public VirtualPet(int food, int water, int play, String name) {
         this.food = food;
         this.water = water;
         this.play = play;
+        this.name = name;
     }
 
     public int getWater() {
@@ -19,5 +21,14 @@ public class VirtualPet {
 
     public int getFood() {
         return food;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getStatus(){
+        outputStr = "Hello I am: "+ getName() + " and my food level is: "
     }
 }
